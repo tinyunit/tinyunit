6855fbd51359e0ebd9babe2d5326a1a3099a231c
Yonggang Luo <luoyonggang@gmail.com>
2016-11-17 23:45:09 +0800

    Add tests for minunit.

--- minunit.h
+++ minunit.h
@@ -28,34 +28,14 @@
 #endif
 
 #if defined(_WIN32)
-#include <Windows.h>
+
 #if defined(_MSC_VER) && _MSC_VER < 1900
   #define snprintf _snprintf
   #define __func__ __FUNCTION__
 #endif
 
-#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+#endif /* _WIN32 */
 
-/* Change POSIX C SOURCE version for pure c99 compilers */
-#if !defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 200112L
-#undef _POSIX_C_SOURCE
-#define _POSIX_C_SOURCE 200112L
-#endif
-
-#include <unistd.h>	/* POSIX flags */
-#include <time.h>	/* clock_gettime(), time() */
-#include <sys/time.h>	/* gethrtime(), gettimeofday() */
-#include <sys/resource.h>
-#include <sys/times.h>
-
-#if defined(__MACH__) && defined(__APPLE__)
-#include <mach/mach.h>
-#include <mach/mach_time.h>
-#endif
-
-#else
-#error "Unable to define timers for an unknown OS."
-#endif
 
 #include <stdio.h>
 #include <math.h>
@@ -66,21 +46,24 @@
 #define MINUNIT_EPSILON 1E-12
 
 /*  Misc. counters */
-static int minunit_run = 0;
-static int minunit_assert = 0;
-static int minunit_fail = 0;
-static int minunit_status = 0;
+extern int minunit_run;
+extern int minunit_assert;
+extern int minunit_fail;
+extern int minunit_status;
 
 /*  Timers */
-static double minunit_real_timer = 0;
-static double minunit_proc_timer = 0;
-
-/*  Last message */
-static char minunit_last_message[MINUNIT_MESSAGE_LEN];
+extern double minunit_real_timer;
+extern double minunit_proc_timer;
 
 /*  Test setup and teardown function pointers */
-static void (*minunit_setup)() = NULL;
-static void (*minunit_teardown)() = NULL;
+extern void (*minunit_setup)();
+extern void (*minunit_teardown)();
+
+extern char* minunit_get_last_message();
+extern double mu_timer_real();
+extern double mu_timer_cpu();
+
+#define minunit_last_message minunit_get_last_message()
 
 /*  Definitions */
 #define MU_TEST(method_name) static void method_name()
@@ -195,169 +178,6 @@ static void (*minunit_teardown)() = NULL;
 	}\
 )
 
-/*
- * The following two functions were written by David Robert Nadeau
- * from http://NadeauSoftware.com/ and distributed under the
- * Creative Commons Attribution 3.0 Unported License
- */
-
-/**
- * Returns the real time, in seconds, or -1.0 if an error occurred.
- *
- * Time is measured since an arbitrary and OS-dependent start time.
- * The returned real time is only useful for computing an elapsed time
- * between two calls to this function.
- */
-static double mu_timer_real( )
-{
-#if defined(_WIN32)
-	/* Windows 2000 and later. ---------------------------------- */
-	LARGE_INTEGER Time;
-	LARGE_INTEGER Frequency;
-	
-	QueryPerformanceFrequency(&Frequency);
-	QueryPerformanceCounter(&Time);
-	
-	Time.QuadPart *= 1000000;
-	Time.QuadPart /= Frequency.QuadPart;
-	
-	return (double)Time.QuadPart / 1000000.0;
-
-#elif (defined(__hpux) || defined(hpux)) || ((defined(__sun__) || defined(__sun) || defined(sun)) && (defined(__SVR4) || defined(__svr4__)))
-	/* HP-UX, Solaris. ------------------------------------------ */
-	return (double)gethrtime( ) / 1000000000.0;
-
-#elif defined(__MACH__) && defined(__APPLE__)
-	/* OSX. ----------------------------------------------------- */
-	static double timeConvert = 0.0;
-	if ( timeConvert == 0.0 )
-	{
-		mach_timebase_info_data_t timeBase;
-		(void)mach_timebase_info( &timeBase );
-		timeConvert = (double)timeBase.numer /
-			(double)timeBase.denom /
-			1000000000.0;
-	}
-	return (double)mach_absolute_time( ) * timeConvert;
-
-#elif defined(_POSIX_VERSION)
-	/* POSIX. --------------------------------------------------- */
-#if defined(_POSIX_TIMERS) && (_POSIX_TIMERS > 0)
-	{
-		struct timespec ts;
-#if defined(CLOCK_MONOTONIC_PRECISE)
-		/* BSD. --------------------------------------------- */
-		const clockid_t id = CLOCK_MONOTONIC_PRECISE;
-#elif defined(CLOCK_MONOTONIC_RAW)
-		/* Linux. ------------------------------------------- */
-		const clockid_t id = CLOCK_MONOTONIC_RAW;
-#elif defined(CLOCK_HIGHRES)
-		/* Solaris. ----------------------------------------- */
-		const clockid_t id = CLOCK_HIGHRES;
-#elif defined(CLOCK_MONOTONIC)
-		/* AIX, BSD, Linux, POSIX, Solaris. ----------------- */
-		const clockid_t id = CLOCK_MONOTONIC;
-#elif defined(CLOCK_REALTIME)
-		/* AIX, BSD, HP-UX, Linux, POSIX. ------------------- */
-		const clockid_t id = CLOCK_REALTIME;
-#else
-		const clockid_t id = (clockid_t)-1;	/* Unknown. */
-#endif /* CLOCK_* */
-		if ( id != (clockid_t)-1 && clock_gettime( id, &ts ) != -1 )
-			return (double)ts.tv_sec +
-				(double)ts.tv_nsec / 1000000000.0;
-		/* Fall thru. */
-	}
-#endif /* _POSIX_TIMERS */
-
-	/* AIX, BSD, Cygwin, HP-UX, Linux, OSX, POSIX, Solaris. ----- */
-	struct timeval tm;
-	gettimeofday( &tm, NULL );
-	return (double)tm.tv_sec + (double)tm.tv_usec / 1000000.0;
-#else
-	return -1.0;		/* Failed. */
-#endif
-}
-
-/**
- * Returns the amount of CPU time used by the current process,
- * in seconds, or -1.0 if an error occurred.
- */
-static double mu_timer_cpu( )
-{
-#if defined(_WIN32)
-	/* Windows -------------------------------------------------- */
-	FILETIME createTime;
-	FILETIME exitTime;
-	FILETIME kernelTime;
-	FILETIME userTime;
-
-	/* This approach has a resolution of 1/64 second. Unfortunately, Windows' API does not offer better */
-	if ( GetProcessTimes( GetCurrentProcess( ),
-		&createTime, &exitTime, &kernelTime, &userTime ) != 0 )
-	{
-		ULARGE_INTEGER userSystemTime;
-		memcpy(&userSystemTime, &userTime, sizeof(ULARGE_INTEGER));
-		return (double)userSystemTime.QuadPart / 10000000.0;
-	}
-
-#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
-	/* AIX, BSD, Cygwin, HP-UX, Linux, OSX, and Solaris --------- */
-
-#if defined(_POSIX_TIMERS) && (_POSIX_TIMERS > 0)
-	/* Prefer high-res POSIX timers, when available. */
-	{
-		clockid_t id;
-		struct timespec ts;
-#if _POSIX_CPUTIME > 0
-		/* Clock ids vary by OS.  Query the id, if possible. */
-		if ( clock_getcpuclockid( 0, &id ) == -1 )
-#endif
-#if defined(CLOCK_PROCESS_CPUTIME_ID)
-			/* Use known clock id for AIX, Linux, or Solaris. */
-			id = CLOCK_PROCESS_CPUTIME_ID;
-#elif defined(CLOCK_VIRTUAL)
-			/* Use known clock id for BSD or HP-UX. */
-			id = CLOCK_VIRTUAL;
-#else
-			id = (clockid_t)-1;
-#endif
-		if ( id != (clockid_t)-1 && clock_gettime( id, &ts ) != -1 )
-			return (double)ts.tv_sec +
-				(double)ts.tv_nsec / 1000000000.0;
-	}
-#endif
-
-#if defined(RUSAGE_SELF)
-	{
-		struct rusage rusage;
-		if ( getrusage( RUSAGE_SELF, &rusage ) != -1 )
-			return (double)rusage.ru_utime.tv_sec +
-				(double)rusage.ru_utime.tv_usec / 1000000.0;
-	}
-#endif
-
-#if defined(_SC_CLK_TCK)
-	{
-		const double ticks = (double)sysconf( _SC_CLK_TCK );
-		struct tms tms;
-		if ( times( &tms ) != (clock_t)-1 )
-			return (double)tms.tms_utime / ticks;
-	}
-#endif
-
-#if defined(CLOCKS_PER_SEC)
-	{
-		clock_t cl = clock( );
-		if ( cl != (clock_t)-1 )
-			return (double)cl / (double)CLOCKS_PER_SEC;
-	}
-#endif
-
-#endif
-
-	return -1;		/* Failed. */
-}
 
 #ifdef __cplusplus
 }
